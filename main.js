8597d3db0ef3ffbf1a249172022e2853a8c7cf8e
UĞURCAN YAŞ <97681426+uydevops@users.noreply.github.com>
2024-03-27 12:17:00 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    // Merchant bilgileri
+    var merchantConfig = {
+        id: '447681',
+        key: '5BW2C4P28TykhcRF',
+        salt: 'Rj6LRkEBuhq629Ke'
+    };
+
+    // Ödeme seçenekleri
+    var paymentConfig = {
+        okUrl: "<?php echo $successUrl; ?>",
+        failUrl: "<?php echo $errorUrl; ?>",
+        testMode: "0",
+        non3d: "0",
+        lang: "tr",
+        non3dTestFailed: "0",
+        type: "card",
+        currency: "TL",
+        installmentCount: "0"
+    };
+
+    // Kullanıcı bilgileri
+    var userInfo = {
+        basket: '<?php echo $user_basket; ?>',
+        oid: "<?php echo $merchant_oid; ?>",
+        ip: "<?php echo $user_ip; ?>",
+        email: "<?php echo $email; ?>"
+    };
+
+    function calculateToken() {
+        var paymentAmount = $('#payment_amount').val();
+        var hashStr = merchantConfig.id + userInfo.ip + userInfo.oid + userInfo.email + paymentAmount + paymentConfig.type + paymentConfig.installmentCount + paymentConfig.currency + paymentConfig.testMode + paymentConfig.non3d + merchantConfig.salt;
+        var base64Encode = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA256(hashStr, merchantConfig.key));
+        var token = base64Encode;
+
+        $('#paytr_token').val(token);
+        $('#payment_amount').val(paymentAmount);
+    }
+
+    // Ödeme miktarı değiştiğinde veya sayfa yüklendiğinde token hesapla
+    $('#payment_amount').on('input', calculateToken);
+    $(window).on('load', calculateToken);
+});
+
+
+
+//Uğurcan Yaş #Altf4 Yazılım 27.03.2024
